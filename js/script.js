16c910d9a47eb8cc0b04774fd2d68e79c3ec3b8d
Temple Naylor <templenaylor@gmail.com>
2017-07-01 20:30:40 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
     const API = 'https://api.unsplash.com/';
 
 
-
     // Random Photo Ajax Call
 
     let randomPhoto = API + 'photos/random/?' + client_id;
@@ -13,15 +12,13 @@ $(document).ready(function () {
 
     $("#newRB").click(function () {
         $.getJSON(randomPhoto, function (response) {
-            console.log(response);
+
             let randomPhoto2 = response.urls.regular;
             // let randomTitle = response.location.title;
             let download = response.links.download + "?force=true";
 
-
             document.getElementById('preview').src = randomPhoto2;
             // document.getElementById('randomTitle').innerHTML = randomTitle;
-
 
             // Create a link to be clicked by the download button
             link = document.createElement('a');
@@ -38,61 +35,73 @@ $(document).ready(function () {
 
 
 
-
     // Search for Photos
 
     $('form').submit(function (e) {
         e.preventDefault();
+        var $searchField = $('#search');
+        var $submitButton = $('#submit');
+
         // API Calls
         let input = document.getElementById("search").value;
-        let $submitButton = $('#submit');
+        // let $submitButton = $('#submit');
         let searchPhoto = API + 'search/photos?' + client_id + '&page=1&query=' + input;
-
+        $searchField.prop("disabled", true);
+        $submitButton.attr("disabled", true).val("searching...");
 
         // Ajax part
         $.getJSON(searchPhoto, function (response) {
 
             // Create beginning of Bootstrap card
-            let photoHTML = '<div class="col-12 col-sm-6">'
+            let photoHTML = '<div class="col-12 col-sm-12">';
 
-            // Loop over each response photo, putting it into a unique card
-            $.each(response.results, function (i, photo) {
-                // Card background
-                let photoBackground = photo.urls.regular;
-                // Download link
-                let download = photo.links.download + "?force=true";
-                // Create a link to be clicked by the download button
-                link = document.createElement('a');
-                link.href = download;
-                link.download = 'Download.jpg';   // The file name suggestion for the user.
-                document.body.appendChild(link);
-
-                // Add each card element 
-                photoHTML += '<article class="card animated fadeInLeft text-center">';
-                photoHTML += '<img class="card-img-top img-responsive preview" src=' + photoBackground + '/>';
+            if (response.total == 0) {
+                photoHTML += '<div class="card card-outline-danger text-center" style="width: 20rem;">';
                 photoHTML += '<div class="card-block">';
-                photoHTML += '<h4 class="card-title" id="randomTitle"></h4>';
-                photoHTML += '<button type="button" class="btn btn-outline-primary common_class" id="div' + i + '">Download</button>'; // Create unique ID
-                photoHTML += '</article>';
+                photoHTML += '<blockquote class="card-blockquote"';
+                photoHTML += '<p>Sorry, nothing came up. How about trying an actual word?</p>';
+                photoHTML += '</blockquote>';
+                photoHTML += '</div>';
+                photoHTML += '</div>';
+            } else {
+                // Loop over each response photo, putting it into a unique card
+                $.each(response.results, function (i, photo) {
+                    // Card background
+                    let photoBackground = photo.urls.regular;
+                    // Download link
+                    let download = photo.links.download + "?force=true";
 
-            })
+                    // Create a link to be clicked by the download button
+                    link = document.createElement('a');
+                    link.href = download;
+                    link.download = 'Download.jpg';   // The file name suggestion for the user.
+                    document.body.appendChild(link);
+
+                    // Add each card element 
+                    photoHTML += '<article class="card indvDisplay text-center">';
+                    photoHTML += '<img class="card-img-top img-responsive preview" src=' + photoBackground + '/>';
+                    photoHTML += '<div class="card-block">';
+                    photoHTML += '<h4 class="card-title" id="randomTitle"></h4>';
+                    photoHTML += '<button type="button"  class="btn btn-outline-success common_class" name=' + download + ' id="div' + i + '">Download</button>'; // Create unique ID
+                    photoHTML += '</article>';
+                })
+            }
+
             // End Card
             photoHTML += '</div>';
+
             // Put each card into a div
-            $('#testing').html(photoHTML);
-            // $(/* ID here */).click(function () {
-            //     // Call download link
-            //     link.click();
-            // });
-            $(document).on('click', '.common_class', function () {
-                currently_clicked_id = $(this).attr('id');
+            $('#photoDisplay').html(photoHTML);
+            // Disable search field
+            $searchField.prop("disabled", false);
+            $submitButton.attr("disabled", false).val("Search");
+
+            $('.common_class').click(function (e) {
+                link.href = e.target.name;
                 link.click();
             });
         })
     })
-
-
-
 
 
 
